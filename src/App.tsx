7714ecc04ed680319c4bbc38f5bf5ed2a8c6dbc8
1carlosrenan <carlosrenan96@gmail.com>
2022-11-20 11:51:54 -0300

    add area to insert new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import { Item } from './types/Item';
 import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -9,17 +10,27 @@ const App = () => {
     {id: 2, name: 'Comprar feijão', done: false},
   ])
 
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList)
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header> Lista de Tarefas </C.Header>
 
-        {/* Área de adicionar nova tarefa*/}
+        <AddArea onEnter={handleAddTask} />
+
           {list.map((item, index) => (
             <ListItem key={index} item={item}/>
           ))}
 
-        {/* */}
       </C.Area>
     </C.Container>
   );
